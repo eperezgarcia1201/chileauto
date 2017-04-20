564246d8767bcad30aa74d84df81c470aff611ec
eperezgarcia1201 <admin@aprode.org>
2017-04-20 00:04:36 -0500

    routes for parts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   resources :users do 
-  	resources :companies, only: [:new, :create,  :edit, :update, :show] 
+  	resources :companies, only: [:new, :create,  :edit, :update, :show] do
   		resources :parts
+  	end
   end
 get 'companies', to: 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
